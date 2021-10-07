c4d2247c4166172af282b9dc7b69b57a7b893dc1
aleemon <a.leemon7@gmail.com>
2021-10-07 17:17:33 +1100

    Package deoendencies updated and Roxygen2 help description added.

--- R/nemwebR_archive_tradingload.R
+++ R/nemwebR_archive_tradingload.R
@@ -1,13 +1,30 @@
+#' Retrieve archived TRADINGLOAD data
+#'
+#' This function returns one month of TRADINGLOAD data from AEMO's NEMWeb as specified by the datestring argument
+#'
+#' TRADINGLOAD data contains historical 30-minute generation quantities for all scheduled and non-scheduled generators in the NEM.
+#'
+#' Archive data is available from July 2009 up to approximately one month ago. In order to retrieve newer data you will need to use the nemwebR_current_TRADINGLOAD function.
+#'
+#'
+#' @param datestring integer of the form YYYYMMDD
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' nemwebR_archive_tradingload(20210101)
+#'
 nemwebR_archive_tradingload <- function(datestring) {
 
   temp <- tempfile()
-  download.file(url = str_c(
+  utils::download.file(url = stringr::str_c(
     "https://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/",
-    str_sub(datestring, start = 1, end = 4),
+    stringr::str_sub(datestring, start = 1, end = 4),
     "/MMSDM_",
-    str_sub(datestring, start = 1, end = 4),
+    stringr::str_sub(datestring, start = 1, end = 4),
     "_",
-    str_sub(datestring, start = 5, end = 6),
+    stringr::str_sub(datestring, start = 5, end = 6),
     "/MMSDM_Historical_Data_SQLLoader/DATA/",
     "PUBLIC_DVD_TRADINGLOAD_",
     datestring,
@@ -15,13 +32,13 @@ nemwebR_archive_tradingload <- function(datestring) {
     destfile = temp, mode = "wb")
 
 
-  data_file <- read.csv(unzip(temp), header = FALSE)
+  data_file <- utils::read.csv(utils::unzip(temp), header = FALSE)
 
 
   ## Dump the files from the hard drive
   unlink(temp)
 
-  unlink(str_c(
+  unlink(stringr::str_c(
     "PUBLIC_DVD_TRADINGLOAD_",
     datestring,
     "0000.csv")
@@ -30,7 +47,7 @@ nemwebR_archive_tradingload <- function(datestring) {
 
   colnames(data_file) <- data_file[2, ]
   data_file <- data_file[-c(1:2), -c(1:4)]
-  data_file <- head(data_file, -1)
+  data_file <- utils::head(data_file, -1)
 
 
   data_file$SETTLEMENTDATE <- as.POSIXct(data_file$SETTLEMENTDATE,
@@ -41,7 +58,7 @@ nemwebR_archive_tradingload <- function(datestring) {
                                       tz = "Australia/Brisbane",
                                       format = "%Y/%m/%d %H:%M:%S")
 
-  data_file <- data_file %>% mutate(across(.cols = c(2, 4:15, 17:20), .fns = as.numeric))
+  data_file <- dplyr::mutate(data_file, dplyr::across(.cols = c(2, 4:15, 17:20), .fns = as.numeric))
 
 
   return(data_file)
